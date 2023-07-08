11960315063e8d39c9d1e502304d744fe4f0027c
Jose Helio <joseheliofvrb@gmail.com>
2023-07-08 16:57:45 -0300

    Tentativa de execução do exercicio3

--- 1 - Maratona/3 - Exercicio/index.js
+++ 1 - Maratona/3 - Exercicio/index.js
@@ -1,15 +1,6 @@
-const pista = []
+let posicoes = [2,2,2,2]
 
-const corrida = "||----------------------CORRIDA---------------------||"
-const fimdacorrida = "||--------------------------------------------------||"
-let cavalinho1 = "||..................................................||"
-let cavalinho2 = "||..................................................||"
-let cavalinho3 = "||..................................................||"
-let cavalinho4 = "||..................................................||"
+function preenche() {
 
-console.log(corrida);
-console.log(cavalinho1);
-console.log(cavalinho2);
-console.log(cavalinho3);
-console.log(cavalinho4);
-console.log(fimdacorrida);
+    
+}
